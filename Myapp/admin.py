f1b2fcb8bcc382fe5a1771f1fc30d4d43034cb50
Holmes-R <hariharanrameshbabu2004@gmail.com>
2025-01-05 11:28:10 +0530

    Created Models

--- Myapp/admin.py
+++ Myapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Blood,Cylinder,Doctor,Hospital,Availability,Organ
-# Register your models here.
 
 admin.site.register(Hospital)
 admin.site.register(Doctor)
